9f1539a168175c28f9faa7a1e535ccd7897083fb
let <kyle.tran.le@gmail.com>
2022-10-28 13:45:07 -0700

    backend structure fixed

--- marvel-tinder-frontend/src/App.js
+++ marvel-tinder-frontend/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   <>
     <Header/>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
       <Route path="/characterindex" element={<CharacterIndex />} />
       <Route path="/charactershow" element={<CharacterShow />} />
       <Route path="/characternew" element={<CharacterNew />} />
