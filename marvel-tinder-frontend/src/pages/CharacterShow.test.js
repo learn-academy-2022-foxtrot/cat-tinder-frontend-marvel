9017e8d540f1689222a95c8c5ae80edd17e12d7b
let <kyle.tran.le@gmail.com>
2022-10-31 13:57:08 -0700

    saving

--- marvel-tinder-frontend/src/pages/CharacterShow.test.js
+++ marvel-tinder-frontend/src/pages/CharacterShow.test.js
@@ -1,24 +1,18 @@
-// import { render, screen } from "@testing-library/react"
-// import CharacterShow from "./CharacterShow"
-// import mockCharacters from "../mockData"
-// import {MemoryRouter, Route, Routes} from "react-router-dom"
-import { render } from "@testing-library/react"
-import CharacterShow from "./CharacterShow"
+import { render, screen } from '@testing-library/react'
+import CharacterShow from './CharacterShow'
+import mockCharacters from "../mockData"
+import { MemoryRouter, Route, Routes } from "react-router-dom"
 
 describe("<CharacterShow />", () => {
-    it("renders without crashing", () => {})
-  })
-
-// describe("<CharacterShow />", () => {
-//   it("renders without crashing", () => {
-//     render (
-//       <MemoryRouter initialEntries={["/Charactershow/1"]}>
-//         <Routes>
-//           <Route path="/Charactershow/:id" element={<CharacterShow Characters={ mockCharacters }/>} />
-//         </Routes>
-//       </MemoryRouter>
-//     )
-//     const showRender = screen.getByText(/sunshine/i)
-//     expect(showRender).toHaveTextContent("Hobbies: sunshine and warm spots")
-//   })
-// })
+    test("renders without crashing", () => {
+        render (
+            <MemoryRouter initialEntries={["/Charactershow/1"]}>
+                <Routes>
+                    <Route path="/Charactershow/:id" element={<CharacterShow Characters={ mockCharacters }/>} />
+                </Routes>
+            </MemoryRouter>
+        )
+        const showRender = screen.getByText([/sunshine/i)
+        expect(showRender).toHaveTextContent("sunshine and warm spots")
+    })
+})
