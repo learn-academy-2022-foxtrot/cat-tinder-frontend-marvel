8e44d1f73f880b1f0f5db893dc877a65e34d1ca5
let <kyle.tran.le@gmail.com>
2022-10-30 19:23:10 -0700

    saving

--- marvel-tinder-frontend/src/pages/CharacterIndex.js
+++ marvel-tinder-frontend/src/pages/CharacterIndex.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { Card, CardBody, Button } from "reactstrap"
+import {NavLink} from "react-router-dom"
 
-const CharacterIndex = () => {
-
-    return(
-        <> Character Index </>
-    )
+const CharacterIndex = ({ characters }) => {
+  return(
+    <main>
+        <>
+        <h1>Get ready to match with your Super Hero</h1>
+        {characters?.map((character, index) => {
+            return(
+            <>
+                <Card 
+                style={{ width: '18rem' }}
+                key={index}
+                >
+                <img alt={`profile of a character named ${character.name}`} src={character.image} />
+                <CardBody>
+                    <NavLink to={`/charactershow/${character.id}`}>
+                    {character.name}
+                    </NavLink>
+                </CardBody>
+                </Card>
+            </>
+            )
+        })}
+        </>
+    </main>
+  )
 }
 
 export default CharacterIndex
--- marvel-tinder-frontend/src/pages/CharacterShow.js
+++ marvel-tinder-frontend/src/pages/CharacterShow.js
@@ -1,10 +1,41 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap"
 
-const CharacterShow = () => {
 
-    return(
-        <> Character Show </>
-    )
+const CharacterShow = ({characters}) => {
+  const { id } = useParams()
+  let showcharacter = characters?.find(character => character.id === +id)
+  return(
+    <>
+      <h1>Thanks for checking out my profile</h1>
+      {showcharacter && 
+        <Card 
+        style={{ width: '18rem' }}
+        >
+          <img src={showcharacter.image} alt="image of eligible feline"/>
+          <CardBody>
+            <CardTitle tag="h5">
+              {showcharacter.name}
+            </CardTitle>
+            <CardSubtitle
+              className="mb-2 text-muted"
+              tag="h6"
+            >
+              Age: {showcharacter.age}
+            </CardSubtitle>
+            <CardSubtitle
+              className="mb-2 text-muted"
+              tag="h6"
+            >
+              Hobbies: {showcharacter.enjoys}
+
+            </CardSubtitle>
+          </CardBody>
+        </Card>
+      }
+    </>
+  )
 }
 
 export default CharacterShow
